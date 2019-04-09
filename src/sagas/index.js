0a9d6be83e50b1d0351e70866ae676f8ac5a645f
pradeep-gururani <pradeep@excellencetechnnologies.in>
2019-04-09 19:35:06 +0530

    redux signup form polling system

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,9 +1,33 @@
-import {put,call, all} from 'redux-saga/effects';
-  function* actionWatcher() {
-       yield call('SAGA_USER_SIGNUP')
-  }
-  export default function* rootSaga() {
-     yield all([
-     actionWatcher(),
-     ]);
-  }
+// import { takeEvery, all, call, put } from 'redux-saga/effects';
+
+// async function* submitForm(data){
+//   try{
+//     let url =`https://secure-refuge-14993.herokuapp.com/add_user?username=${data.username}&password=${data.password}&role=${data.role}`;
+//     let response = await fetch(url, {
+//       method: 'POST'    
+//   });
+//   let responseJson = await response.json();
+//   return responseJson;
+// }catch(error){
+//     console.log(error);
+// }
+// }
+
+// function* callSubmit(action) {
+//   const result = yield call(submitForm, action);
+//   if(result.errors){
+//     yield put({type: 'REQUEST_FAILED', result});
+//   }
+//   else{
+//     yield put({type: 'REQUEST_SUCCESSFUL', result});
+//   }
+//   yield put({ type: '', result });
+// }
+
+// export default function* rootSaga() {
+//   yield [
+//     all([
+//           submitForm()
+//         ])
+//   ];
+// }
